5fb582542b10db031d598a32e2cc309c4957caaf
Vero <vskurtmullaeva@stud.kantiana.ru>
2023-10-19 18:00:17 +0200

    Done

---
+++ Lab_3/main.cpp
@@ -0,0 +1,106 @@
+#include "Array3D.cpp"
+
+int main() {
+    setlocale(LC_ALL,"Rus");
+
+    // Cube
+    // x,y,z - size
+    int x = 2, y = 3, z = 2, index = 1;
+
+    Array3D<int> cube(x, y, z);
+
+    // Filling 
+    for (int i = 0; i < x; ++i) {
+        for (int j = 0; j < y; ++j) {
+            for (int k = 0; k < z; ++k) {
+                cube(i,j,k) = index;
+                index++;
+            }
+        }
+    }
+
+    std::cout << "В виде строки разрезаем куб в точке x=0 " << std::endl;
+
+    for (int value : cube.GetValuesX(0)) {
+        std::cout << value << " ";
+    }
+
+    std::cout<<std::endl<<"В виде прямогульника разрезаем куб в x=0 "<<std::endl;
+
+    std::vector<std::vector<int>> test1 = cube.GetValuesX2D(0);
+    for( auto& i : test1){
+        for (auto& j: i) {
+            std::cout << j << ' ';
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout<<"В виде строки разрезаем куб в y=2 "<<std::endl;
+
+    for (int value : cube.GetValuesY(2)) {
+        std::cout << value << " ";
+    }
+
+    std::cout<<std::endl<<"Разрезаем в виде прямоугольника куб в y=2 "<<std::endl;
+
+    std::vector<std::vector<int>> test2 = cube.GetValuesY2D(2);
+    for( auto& i : test2){
+        for (auto& j: i) {
+            std::cout << j << ' ';
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout<<"Разрезаем в виде строки куб в z = 1 "<<std::endl;
+
+    for (int value : cube.GetValuesZ(1)) {
+        std::cout << value << " ";
+    }
+
+    std::cout<<std::endl<<"Разрезаем в виде прямоугольника куб в z=1 "<<std::endl;
+
+    std::vector<std::vector<int>> test3 = cube.GetValuesZ2D(1);
+    for( auto& i : test3){
+        for (auto& j: i) {
+            std::cout << j << ' ';
+        }
+        std::cout << std::endl;
+    }
+
+    //утанавливаем новые значения в точке x=0
+    cube.SetValues0(0,{-1, -2, -3, -4, -5 ,-6});
+
+    std::cout<<"После изменения - срез куба в x = 0 - в виде строки "<<std::endl;
+
+    for (int value : cube.GetValuesX(0)) {
+        std::cout << value << " ";
+    }
+
+    std::cout<<std::endl<<"Срез куба в x = 0 и y = 1"<<std::endl;
+
+    for (int value : cube.GetValuesXY(0,1)) {
+        std::cout << value << " ";
+    }
+
+    cube.SetValues01(0,1,{7,7});
+
+    std::cout<<std::endl<<"После изменения срез куба в x = 0 и y = 1"<<std::endl;
+
+    for (int value : cube.GetValuesXY(0,1)) {
+        std::cout << value << " ";
+    }
+
+    cube.Fill(5,5,5,1);
+
+    std::cout<<std::endl<<"После заполнения 1 срез куба в y = 2 - в виде прямоугольника"<<std::endl;
+
+    std::vector<std::vector<int>> test4 = cube.GetValuesY2D(2);
+    for( auto& i : test4){
+        for (auto& j: i) {
+            std::cout << j << ' ';
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
